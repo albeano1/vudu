6fa8ec919ea49a9edc2f207463a0b91fb2379138
Sean N <47581080+albeano1@users.noreply.github.com>
2023-06-22 18:33:08 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello NC" << endl;
+    cout << "what is your age" << endl;
+    cin >> age;
+    cout << "you are" << age << endl;
+
+
+
+    return 0;
+}
